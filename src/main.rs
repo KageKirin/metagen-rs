f37da5671e1bce2af1967960c38739d7e5bf61e3
Chris Helmich <kagekirin@gmail.com>
2021-10-21 12:18:15 +0900

    Implement metafile generation as straightforward xxhash

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
+use std::env;
+use std::io::Write;
+use std::path::PathBuf;
+
+use argh::FromArgs;
+use twox_hash::xxh3::hash128_with_seed;
+use twox_hash::xxh3::hash64;
+
+fn default_seed() -> String {
+    String::from(env::current_dir().unwrap().to_str().unwrap())
+}
+
+#[derive(FromArgs)]
+/// Generate Unity .meta files
+struct Args {
+    /// seed string, e.g. package name
+    #[argh(option, short = 's', default = "default_seed()")]
+    seed: String,
+
+    /// files to generate .meta files for
+    #[argh(positional)]
+    files: Vec<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = argh::from_env();
+    println!("seed: {}", args.seed);
+
+    let seed: u64 = hash64(args.seed.as_bytes());
+    println!("seed hash: {:x}", seed);
+
+    for f in args.files {
+        if f.exists() {
+            let guid = hash128_with_seed(f.to_str().unwrap().as_bytes(), seed);
+            println!("{} -> {:x}", f.to_str().unwrap(), guid);
+            generate_meta_file(f, guid);
+        }
+    }
+}
+
+fn generate_meta_file(filename: PathBuf, guid: u128) {
+    let metafilename = PathBuf::from(format!("{}.meta", &filename.to_string_lossy()));
+    println!("{}", &metafilename.to_string_lossy());
+
+    let data = format!(
+        "fileFormatVersion: 2
+guid: {:x}
+{}",
+        guid,
+        r#"MonoImporter:
+  externalObjects: {}
+  serializedVersion: 2
+  defaultReferences: []
+  executionOrder: 0
+  icon: {instanceID: 0}
+  userData: 
+  assetBundleName: 
+  assetBundleVariant:
+"#
+    );
+    println!("{}", data);
+    let mut file = std::fs::File::create(&metafilename).expect("create failed");
+    file.write_all(data.as_bytes()).expect("write failed");
 }
